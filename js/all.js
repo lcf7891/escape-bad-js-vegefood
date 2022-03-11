33823bbbb7e0ab07cccda8d0afdbef9ad452e34a
Allen <lcf7891@gmail.com>
2022-03-12 00:03:24 +0800

    完成修改

--- js/all.js
+++ js/all.js
@@ -1,53 +1,55 @@
-// TODO: 修正 ESLint 錯誤、補上分號、前輩說要改單引號 QQ
+/* eslint-disable valid-jsdoc */
+/**
+ * TODO: 修正 ESLint 錯誤、補上分號、前輩說要改單引號 QQ
+ */
+
+// 外部資料位址
 const url='https://hexschool.github.io/js-filter-data/data.json';
+
+// 表單 DOM 元素
+const table=document.querySelector('.table-content');
+
+// 按鈕 DOM 元素
+const filter=document.querySelector('.filter');
+
+// 監聽按鈕觸發篩選函式渲染
+filter.addEventListener('click', filterCategory);
+
+// 外部資料存放位置
 let data;
 
+// 篩選渲染資料位置
+let showData=[];
+
+// axios 套件將外部資料導入
 axios.get(url)
     .then(function(res) {
       data=res.data.filter((a)=>a.作物名稱);
-      // TODO: 之後拆成 renderData 函式
-      let str='';
-      data.forEach((b, index)=>{
-        // TODO: 改成 ES6 的 Template Literals (字面字串符)
-        const content=
-          `<tr>
-            <td>${b.作物名稱}</td>
-            <td>${b.市場名稱}</td>
-            <td>${b.上價}</td>
-            <td>${b.中價}</td>
-            <td>${b.下價}</td>
-            <td>${b.平均價}</td>
-            <td>${b.交易量}</td>
-          </tr>`;
-        str+=content;
-      });
-      table.innerHTML=str;
+      renderData();
     });
 
-const table=document.querySelector('.table-content');
-let showData=[];
 
-let category='';
-const filter=document.querySelector('.filter');
-
-filter.addEventListener('click', filterCategory);
-
+/** 觸發按鈕篩選
+ * @param {Event} event
+ */
 function filterCategory(e) {
+  let category='';
   if (e.target.nodeName=='BUTTON') {
     category=e.target.dataset.category;
     showData=data.filter((i)=>{
       return i.種類代碼==category;
     });
-    renderData(i);
+    renderData();
   } else {
     return;
-  }
-}
+  };
+};
 
-function renderData(data) {
+/** 渲染篩選後資料 */
+function renderData() {
   let str='';
-      data.forEach((b, index)=>{
-        const content=
+  showData.forEach((b, index)=>{
+    const content=
           `<tr>
             <td>${b.作物名稱}</td>
             <td>${b.市場名稱}</td>
@@ -57,7 +59,7 @@ function renderData(data) {
             <td>${b.平均價}</td>
             <td>${b.交易量}</td>
           </tr>`;
-        str+=content;
-      });
-      table.innerHTML=str;
+    str+=content;
+  });
+  table.innerHTML=str;
 };
